33d082094c3e6e87c95f3c554d10c26989b3b5f1
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-08-04 01:20:32 +0900

    emailSender 수정

--- src/main/java/com/pjh/share/api/AccountApiController.java
+++ src/main/java/com/pjh/share/api/AccountApiController.java
@@ -2,7 +2,7 @@ package com.pjh.share.api;
 
 
 import com.pjh.share.service.AccountService;
-import com.pjh.share.service.EmailService;
+import com.pjh.share.component.EmailSender;
 import com.pjh.share.web.dto.AccountCreateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +13,12 @@ import java.util.UUID;
 @RestController
 public class AccountApiController {
     private final AccountService accountService;
-    private final EmailService emailService;
+    private final EmailSender emailSender;
     @PostMapping("/join")
     public Long save(@RequestBody AccountCreateRequestDto req) throws Exception{
         String authString= UUID.randomUUID().toString();
         req.setAuthString(authString);
-        emailService.sendAuthMail(req.getEmail(),authString);
+        emailSender.sendAuthMail(req.getEmail(),authString);
         return accountService.save(req);
     }
 }
--- src/main/java/com/pjh/share/component/EmailSender.java
+++ src/main/java/com/pjh/share/component/EmailSender.java
@@ -1,4 +1,4 @@
-package com.pjh.share.service;
+package com.pjh.share.component;
 
 
 
@@ -6,26 +6,32 @@ import com.pjh.share.util.EmailHandler;
 import lombok.RequiredArgsConstructor;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
-@RequiredArgsConstructor
-@Service
-public class EmailService {
-    private final JavaMailSender emailSender;
+
+@Component
+public class EmailSender {
+    @Autowired
+    private JavaMailSender emailSender;
+
 
     @Value("${spring.mail.username}")
     private String FROM_ADDRESS;
+
 
     public void sendAuthMail(String to,String authString) throws Exception{
 
         String title="이메일 인증번호입니다";
         EmailHandler emailHandler=new EmailHandler(emailSender);
-        emailHandler.setFrom(FROM_ADDRESS);
+        emailHandler.setFrom("epictroll2018@gmail.com");
         emailHandler.setTo(to);
         emailHandler.setSubject(title);
         String htmlContent="<a href='http://localhost:8080/email/auth/"+authString+"'"+">인증하기</a>";
