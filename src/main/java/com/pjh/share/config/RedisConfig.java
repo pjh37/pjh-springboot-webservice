001ea64d3ffb575e561a5d174960facb2c44d623
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-08-15 23:56:44 +0900

    redis 설정 변경

--- src/main/java/com/pjh/share/config/RedisConfig.java
+++ src/main/java/com/pjh/share/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.pjh.share.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pjh.share.web.dto.SessionUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,11 +10,11 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories
 public class RedisConfig {
     @Value("${spring.redis.host}")
     private String redisHost;
@@ -19,17 +22,25 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
 
+    @Autowired
+    private ObjectMapper mapper;
+
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
         return new LettuceConnectionFactory(redisHost,redisPort);
     }
 
     @Bean
-    public RedisTemplate<?,?> redisTemplate(){
+    public RedisTemplate<String,Object> redisTemplate(){
         RedisTemplate<String,Object> redisTemplate=new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(mapper));//new GenericJackson2JsonRedisSerializer()
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setEnableDefaultSerializer(false);
+        redisTemplate.setEnableTransactionSupport(true);
         return redisTemplate;
     }
 }
