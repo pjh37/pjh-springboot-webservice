065749b9ea5fc3acd836a8b954ac9a9d5a81b3ed
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-09-19 02:12:23 +0900

    postservice 테스트 코드 변경

--- src/test/java/com/pjh/share/service/post/PostService.java
+++ src/test/java/com/pjh/share/service/post/PostService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+
 @RequiredArgsConstructor
 public class PostService {
     private final static Integer PAGE_SIZE=10;
