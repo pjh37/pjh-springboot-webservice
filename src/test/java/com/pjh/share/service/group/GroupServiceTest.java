7cc139b2df41266c98cf367fd4d99fb93c89dc95
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-09-24 00:15:56 +0900

    Mockito로 변경

--- src/test/java/com/pjh/share/service/group/GroupServiceTest.java
+++ src/test/java/com/pjh/share/service/group/GroupServiceTest.java
@@ -3,12 +3,14 @@ package com.pjh.share.service.group;
 import com.pjh.share.component.S3Uploader;
 import com.pjh.share.domain.account.Account;
 import com.pjh.share.domain.account.AccountRepository;
-import com.pjh.share.domain.account.Role;
+
 import com.pjh.share.domain.account.SessionUser;
 import com.pjh.share.domain.file.FileRepository;
 import com.pjh.share.domain.group.Group;
 import com.pjh.share.domain.group.GroupRepository;
+import com.pjh.share.domain.groupaccount.GroupAccount;
 import com.pjh.share.domain.groupaccount.GroupAccountRepository;
+import com.pjh.share.domain.groupaccount.Role;
 import com.pjh.share.service.GroupService;
 import com.pjh.share.web.dto.GroupCreateRequestDto;
 import com.pjh.share.web.dto.GroupPwCheckRequestDto;
@@ -52,13 +54,13 @@ public class GroupServiceTest {
 
     private SessionUser sessionUser;
 
+    private GroupService groupService;
     @BeforeEach
     public void setup(){
-
-
+        groupService=new GroupService(groupAccountRepository,groupRepository
+                ,fileRepository,accountRepository,s3Uploader);
         sessionUser=SessionUser.builder()
                 .name("user")
-                .role(Role.USER)
                 .id(USER_ID)
                 .build();
     }
@@ -67,8 +69,6 @@ public class GroupServiceTest {
     @DisplayName("그룹 생성 테스트")
     public void groupCreate() throws Exception{
         //given
-        GroupService groupService=new GroupService(groupAccountRepository,groupRepository
-                ,fileRepository,accountRepository,s3Uploader);
         Group group=toGroupEntity();
         Account account=toAccountEntity();
         group.setAccount(account);
@@ -85,67 +85,46 @@ public class GroupServiceTest {
         GroupResponseDto groupResponseDto2=groupService.findById(GROUP_ID);
         assertThat(groupResponseDto2.getTitle()).isEqualTo("그룹이름");
     }
-
-    /*
-    @BeforeEach
-    public void setUp(){
-        Long accountId=accountRepository.save(Account.builder().name("유저").password("123")
-                .email("abc").role(Role.USER).authString("abc").build()).getId();
-        sessionUser=SessionUser.builder()
-                .name("유저")
-                .role(Role.USER)
-                .id(accountId)
-                .build();
-    }
-
-    @Test
-    @DisplayName("그룹 생성 테스트")
-    public void groupCreate() throws Exception{
-        Long savedId=groupService.save(buildGroupDto(),sessionUser);
-        GroupResponseDto dto=groupService.findById(savedId);
-
-        assertThat(dto.getTitle()).isEqualTo("그룹이름");
-        assertThat(dto.getDescription()).isEqualTo("그룹설명");
-    }
-
     @Test
     @DisplayName("그룹 멤버 여부 체크")
     public void groupMemberCheck()throws Exception{
-        Long accountId=accountRepository.save(Account.builder().name("유저2").password("12345")
-                .email("abcdef").role(Role.USER).authString("abcdf").build()).getId();
-        Long savedId=groupService.save(buildGroupDto(),sessionUser);
-        SessionUser newUser=SessionUser.builder()
-                .name("유저2")
-                .role(Role.USER)
-                .id(accountId)
-                .build();
-        boolean groupCreatorCheckResult=groupService.groupMemberCheck(savedId,sessionUser);
-        boolean newUserCheckResult=groupService.groupMemberCheck(savedId,newUser);
+        //given
+        given(groupAccountRepository.existsByAccountIdAndGroupId(USER_ID,GROUP_ID)).willReturn(true);
 
-        assertThat(groupCreatorCheckResult).isEqualTo(true);
-        assertThat(newUserCheckResult).isEqualTo(false);
-    }
+        //when
+        boolean result=groupService.groupMemberCheck(GROUP_ID,sessionUser);
 
-
-    @Test
-    @DisplayName("그룹 비밀번호 맞을때 체크")
-    public void groupPasswordCheckSuccess()throws Exception{
-        Long savedId=groupService.save(buildGroupDto(),sessionUser);
-        boolean result=groupService.groupPwCheck(buildGroupPwCheckRequestDto(savedId,"123"));
-
+        //then
         assertThat(result).isEqualTo(true);
     }
 
     @Test
+    @DisplayName("그룹 비밀번호 맞을때 체크")
+    public void groupPasswordCheckSuccess()throws Exception{
+        //given
+        Group group=toGroupEntity();
+        given(groupRepository.findById(GROUP_ID)).willReturn(Optional.of(group));
+
+        //when
+        boolean result=groupService.groupPwCheck(buildGroupPwCheckRequestDto(GROUP_ID,"123"));
+
+        //then
+        assertThat(result).isEqualTo(true);
+    }
+    @Test
     @DisplayName("그룹 비밀번호 틀렸을때 체크")
     public void groupPasswordCheckFail()throws Exception{
-        Long savedId=groupService.save(buildGroupDto(),sessionUser);
-        boolean result=groupService.groupPwCheck(buildGroupPwCheckRequestDto(savedId,"123456"));
+        //given
+        Group group=toGroupEntity();
+        given(groupRepository.findById(GROUP_ID)).willReturn(Optional.of(group));
 
+        //when
+        boolean result=groupService.groupPwCheck(buildGroupPwCheckRequestDto(GROUP_ID,"123456"));
+
+        //then
         assertThat(result).isEqualTo(false);
     }
 
-     */
     private GroupCreateRequestDto buildGroupDto(Group group){
         GroupCreateRequestDto dto=new GroupCreateRequestDto();
         dto.setTitle(group.getTitle());
@@ -167,7 +146,7 @@ public class GroupServiceTest {
                 .email("abc@naver.com")
                 .password("123123123")
                 .authString("1234556")
-                .role(Role.USER).build();
+                .build();
         return account;
     }
     private Group toGroupEntity(){
