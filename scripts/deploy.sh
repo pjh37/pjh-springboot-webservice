136d5d60d5791a91ede4ab79468573223201ddc3
pjh37 <37110261+pjh37@users.noreply.github.com>
2020-10-12 23:47:44 +0900

    배포스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step1
-PROJECT_NAME=pjh-server
+PROJECT_NAME=pjh-springboot-webservice
 
 echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 구동중인 애플리케이션 pid확인"
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl pjh-server | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl pjh-springboot-webservice | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 애플리케이션 pid : $CURRENT_PID"
 
